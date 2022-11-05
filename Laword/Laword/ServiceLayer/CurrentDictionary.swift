1ad03b337ae1e7bcdac2d7af01e7401e84c56a50
moraWoo <pkstratech@yandex.ru>
2022-11-06 00:50:02 +0500

    Laword/Laword/Apps/Assets.xcassets/Onboard/imageOB1.imageset/Contents.json

---
+++ Laword/Laword/ServiceLayer/CurrentDictionary.swift
@@ -0,0 +1,14 @@
+//
+//  Dictionary.swift
+//  Laword
+//
+//  Created by Ildar Khabibullin on 05.11.2022.
+//
+
+import Foundation
+
+struct  CurrentDictionary {
+    let name: String?
+    let countOfAllWords: Int16?
+    let countOfRemainWords: Int16?
+}
